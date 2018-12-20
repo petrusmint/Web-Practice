225d693de852eef3bad7c7e143de412dde484099
Randolf Sartagoda <randolf.sartagoda@cri.clickablebrand.com>
2018-12-20 15:09:29 +0800

    updated template to use blade syntax

--- content/themes/masterdev2/resources/views/page-home.blade.php
+++ content/themes/masterdev2/resources/views/page-home.blade.php
@@ -1,152 +1,99 @@
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php(the_post())
-	<?php
-		$orderFields = get_post_meta( 15, 'order_fields', true );
-	?>
-  	<section id="banner">
-  		<div class="container">
-  			<div class="wrapper">
-  				<div class="row">
-  					<div class="col-md-5 align-self-md-center">
-  						<div class="img-cont text-center" data-aos="fade-up" data-aos-duration="1000">
-							<?php
-								if(has_post_thumbnail(9)) {
-									$bookImg = wp_get_attachment_image_src(get_post_thumbnail_id(9), 'single-post-thumbnail');
-									echo '<img src="'.$bookImg[0].'" alt="'.get_bloginfo('description').'"/>';
-								}
-							?>
+	@while(have_posts()) @php(the_post())
+	<section id="banner">
+		<div class="container">
+			<div class="wrapper">
+				<div class="row">
+					<div class="col-md-5 align-self-md-center">
+						<div class="img-cont text-center" data-aos="fade-up" data-aos-duration="1000">
+							{!! get_the_post_thumbnail(9, 'single-post-thumbnail', ['style' => 'height:auto!important']) !!}
 						</div>
-  					</div>
-  					<div class="col-md-7 align-self-md-center">
-  						<div class="text-cont text-center" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
-	  						<?php
-								$content_post = get_post(9);
-								$content = $content_post->post_content;
-								$content = apply_filters('the_content', $content);
-								$content = str_replace(']]>', ']]&gt;', $content);
-								echo wpautop($content);
-							?>
+					</div>
+					<div class="col-md-7 align-self-md-center">
+						<div class="text-cont text-center" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
+							{!! get_post(9)->post_content !!}
+							<h2>Written by {{ get_bloginfo("name", "display") }}</h2>
+							<a href="{{ get_post_meta(9, 'buy the book now link', true) }}" target="_blank"
+							   class="btn-style buy-book">{{ get_post_meta(9, 'buy the book now', true) }}</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section><!-- /#banner -->
 
-		  					<h2>Written by {{ get_bloginfo("name", "display") }}</h2>
-		  					<a href="<?php echo get_post_meta(9, 'buy the book now link', true); ?>" target="_blank" class="btn-style buy-book"><?php echo get_post_meta(9, 'buy the book now', true); ?></a>
-		  				</div>
-  					</div>
-  				</div>
-  			</div>
-  		</div>
-    </section>
-    <section id="book-review">
-		<?php
-			$args = array (
-				'posts_per_page'		=> 3,
-				'offset'				=> 0,
-				'orderby'				=> 'date',
-				'order'					=> 'DESC',
-				'post_type'				=> 'bookreviews',
-				'post_status'			=> 'publish',
-				'suppress_filters'		=> true
-			);
-			$posts_array = get_posts($args);
-		?>
-    	<div class="container review-columns" data-aos="fade-up" data-aos-duration="1000">
+	<section id="book-review">
+		<div class="container review-columns" data-aos="fade-up" data-aos-duration="1000">
 			<h2 class="text-center">Book Reviews Columns</h2>
 			<div class="wrapper" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="600">
-    			<div class="row">
-	    			<?php
-	    				foreach ($posts_array as $review) :
-	    					$address = get_post_meta($review->ID,'address', true);
-	    					echo '<div class="col-lg-4 review-wrap">
-	    						  <div class="review-wrapper">
-	    							<div class="bg-cont">
-	    							'.wpautop($review->post_content).'
-	    							</div>
-	    							<h3>'.$review->post_title.'';
-									if(!empty($address)) {
-										echo '<strong>, '.$address.'</strong>';
-									}
-	    					echo  '</h3>
-	    							
-	    						   
-								  </div>
-								  </div>';
-						endforeach;
-	    			?>
-	    		</div>
-    		</div>
-    	</div>
-    	<div class="container review-carousel" data-aos="fade-up" data-aos-duration="1000">
-    		<h2 class="text-center">Book Reviews Carousel</h2>
-    		<div class="wrapper" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="600">
-    			<div id="reviewCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
-	    			<div class="carousel-inner">
-		    			<?php
-		    				$count = 0;
-	                        $status = '';
-	                        $carouIn = '<ol class="carousel-indicators">';
-		    				foreach ($posts_array as $review) :
-		    					$address = get_post_meta($review->ID,'address', true);
-		    					if($count == 0)
-	                            {
-	                              $status = 'active';
-	                            }
-	                            else{
-	                              $status = '';
-	                            }
-	                            $carouIn.= '<li data-target="#reviewCarousel" data-slide-to="'.$count.'" class="'.$status.'"></li>';
-		    					echo '<div class="carousel-item '.$status.'">
-                                    <div class="review-cont">
-                                       '.  wpautop($review->post_content)  .'
-                                       <h3>—'. $review->post_title ;
-                                        if(!empty($address)) {
-                                            echo '<span>, '.$address.'</span>';
-                                        }
-                           		echo '</h3>
-                                    </div>
-                                	</div>';
-							$count++;
-							endforeach;
-							$carouIn.='</ol>';
-		    			?>
-		    		</div>
-		    		<a class="carousel-control-prev" href="#reviewCarousel" data-slide="prev">
+				<div class="row">
+					@foreach ($book_reviews as $review)
+						<div class="col-lg-4 review-wrap">
+							<div class="review-wrapper">
+								<div class="bg-cont">{!! $review['content'] !!}</div>
+								<h3>{{ $review['title'] }}
+									@if(!empty($review['address']))
+										<strong>, {{ $review['address'] }}</strong>
+									@endif
+								</h3>
+							</div>
+						</div>
+					@endforeach
+				</div><!-- /.row -->
+			</div><!-- /.wrapper -->
+		</div><!-- /.review-columns -->
+
+		<div class="container review-carousel" data-aos="fade-up" data-aos-duration="1000">
+			<h2 class="text-center">Book Reviews Carousel</h2>
+			<div class="wrapper" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="600">
+				<div id="reviewCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
+					<div class="carousel-inner">
+						@foreach ($book_reviews as $review)
+							<div class="carousel-item @if ($loop->first) active @endif">
+								<div class="review-cont">
+									{!! wpautop($review['content']) !!}
+									<h3>— {{ $review['title'] }}
+										@if(!empty($review['address']))
+											<span>, {{ $review['address'] }}</span>
+										@endif
+									</h3>
+								</div>
+							</div>
+						@endforeach
+					</div><!-- /.carousel-inner -->
+
+					<a class="carousel-control-prev" href="#reviewCarousel" data-slide="prev">
 						<span class="carousel-control-prev-icon"></span>
 					</a>
 					<a class="carousel-control-next" href="#reviewCarousel" data-slide="next">
 						<span class="carousel-control-next-icon"></span>
 					</a>
-		    		<?php
-                        echo $carouIn;
-                    ?>
-	    		</div>
-    		</div>
-    	</div>
-	</section>
+
+					<ol class="carousel-indicators">
+						@foreach ($book_reviews as $review)
+							<li data-target="#reviewCarousel" data-slide-to="{{ $loop->index }}" @if ($loop->first) class="active" @endif></li>
+						@endforeach
+					</ol>
+				</div><!-- /#reviewCarousel -->
+			</div><!-- /.wrapper -->
+		</div><!-- /.review-carousel -->
+	</section><!-- /#book-review -->
+
 	<section id="book">
 		<div class="container">
 			<div class="wrapper">
-				<h2 class="text-center" data-aos="fade-up" data-aos-duration="1000"><?php echo get_the_title(11); ?></h2>
+				<h2 class="text-center" data-aos="fade-up" data-aos-duration="1000">{{ get_the_title(11) }}</h2>
 				<div class="row">
 					<div class="col-md-4 order-md-12">
 						<div class="img-cont text-center" data-aos="fade-up" data-aos-duration="1000">
-							<?php
-								if(has_post_thumbnail(11)) {
-									$bookImg = wp_get_attachment_image_src(get_post_thumbnail_id(11), 'single-post-thumbnail');
-									echo '<img src="'.$bookImg[0].'" alt="'.get_bloginfo('description').'"/>';
-								}
-							?>
+							{!! get_the_post_thumbnail(11, 'single-post-thumbnail', ['style' => 'height:auto!important']) !!}
 						</div>
 					</div>
 					<div class="col-md-8">
 						<div class="text-cont" data-aos="fade-up" data-aos-duration="1000">
-							<?php
-								$content_post = get_post(11);
-								$content = $content_post->post_content;
-								$content = apply_filters('the_content', $content);
-								$content = str_replace(']]>', ']]&gt;', $content);
-								echo wpautop($content);
-							?>
+							{!! wpautop(get_post(11)->post_content) !!}
 						</div>
 					</div>
 				</div>
@@ -155,25 +102,16 @@
 	</section>
 	<section id="author">
 		<div class="container">
-	        <div class="wrapper">
-	        	<div class="img-cont" data-aos="fade-up" data-aos-duration="1000">
-        			<?php if (has_post_thumbnail( 13 ) ): ?>
-	                    <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( 13 ), 'single-post-thumbnail' ); ?>
-	                    <img src="<?php echo $image[0]; ?>" alt="<?php echo get_bloginfo( 'name' ); ?>">
-	                <?php endif; ?>  
-	            </div>
-	            <div class="text-cont text-center" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
-                    <h2><?php echo get_the_title(13); ?></h2>
-                    <?php
-						$content_post = get_post(13);
-						$content = $content_post->post_content;
-						$content = apply_filters('the_content', $content);
-						$content = str_replace(']]>', ']]&gt;', $content);
-						echo wpautop($content);
-					?>
-                </div>
-	        </div>
-	    </div>
+			<div class="wrapper">
+				<div class="img-cont" data-aos="fade-up" data-aos-duration="1000">
+					{!! get_the_post_thumbnail(13, 'single-post-thumbnail', ['style' => 'height:auto!important']) !!}
+				</div>
+				<div class="text-cont text-center" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
+					<h2>{{ get_the_title(13) }}</h2>
+					{!! wpautop(get_post(13)->post_content) !!}
+				</div>
+			</div>
+		</div>
 	</section>
 	<section id="order">
 		<div class="container">
@@ -182,92 +120,74 @@
 					<div class="col-md-5 book-det">
 						<div class="book-details" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
 							<div class="img-cont">
-								<?php if (has_post_thumbnail( 15 ) ): ?>
-				                    <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( 15 ), 'single-post-thumbnail' ); ?>
-				                    <img src="<?php echo $image[0]; ?>" alt="<?php echo get_bloginfo( 'name' ); ?>">
-				                <?php endif; ?>  
+								{!! get_the_post_thumbnail(15, 'single-post-thumbnail', ['style' => 'height:auto!important']) !!}
 							</div>
 							<div class="book-type">
-								<?php
-									if(!empty($orderFields[0]['hardcover-link'])) {
-										echo '<a href="'.$orderFields[0]['hardcover-link'].'" target="_blank" class="btn-style">Hardcover | '.$orderFields[0]['hardcover-price'].'</a>';
-									}
-									if(!empty($orderFields[0]['paperback-link'])) {
-										echo '<a href="'.$orderFields[0]['paperback-link'].'" target="_blank" class="btn-style">Paperback | '.$orderFields[0]['paperback-price'].'</a>';
-									}
-									if(!empty($orderFields[0]['ebook-link'])) {
-										echo '<a href="'.$orderFields[0]['ebook-link'].'" target="_blank" class="btn-style">E-book | '.$orderFields[0]['ebook-price'].'</a>';
-									}
-									if(!empty($orderFields[0]['kindle-link'])) {
-										echo '<a href="'.$orderFields[0]['kindle-link'].'" target="_blank" class="btn-style">Kindle | '.$orderFields[0]['kindle-price'].'</a>';
-									}
-									if(!empty($orderFields[0]['nook-book-link'])) {
-										echo '<a href="'.$orderFields[0]['nook-book-link'].'" target="_blank" class="btn-style">Nook Book | '.$orderFields[0]['nook-book-price'].'</a>';
-									}
-								?>
+								@isset($order_fields['hardcover-link'])
+									<a href="{{ $order_fields['hardcover-link'] }}" target="_blank" class="btn-style">Hardcover | {{ $order_fields['hardcover-price'] }}</a>
+								@endisset
+
+								@isset($order_fields['paperback-link'])
+									<a href="{{ $order_fields['paperback-link'] }}" target="_blank" class="btn-style">Paperback | {{ $order_fields['paperback-price'] }}</a>
+								@endisset
+
+								@isset($order_fields['ebook-link'])
+									<a href="{{ $order_fields['ebook-link'] }}" target="_blank" class="btn-style">E-book | {{ $order_fields['ebook-price'] }}</a>
+								@endempty
+
+								@isset($order_fields['kindle-link'])
+									<a href="{{ $order_fields['kindle-link'] }}" target="_blank" class="btn-style">Kindle | {{ $order_fields['kindle-price'] }}</a>
+								@endisset
+
+								@isset($order_fields['nook-book-link'])
+									<a href="{{ $order_fields['nook-book-link'] }}" target="_blank" class="btn-style">Nook Book | {{ $order_fields['nook-book-price'] }}</a>
+								@endisset
 							</div>
 							<div class="book-store">
 								<span>Available at</span>
-								<?php
-									if(!empty($orderFields[0]['litfire-link'])) {
-										$litfire = wp_get_attachment_image_src( $orderFields[0]['litfire-image'], 'full', false );
-										echo '<a href="'.$orderFields[0]['litfire-link'].'" target="_blank" class="litfire"><img src="'.$litfire[0].'" alt="litfire"/></a>';
-									}
-									if(!empty($orderFields[0]['amazon-link'])) {
-										$amazon = wp_get_attachment_image_src( $orderFields[0]['amazon-image'], 'full', false );
-										echo '<a href="'.$orderFields[0]['amazon-link'].'" target="_blank" class="amazon"><img src="'.$amazon[0].'" alt="amazon"/></a>';
-									}
-									if(!empty($orderFields[0]['barnes-noble-link'])) {
-										$barnes = wp_get_attachment_image_src( $orderFields[0]['barnes-noble-image'], 'full', false );
-										echo '<a href="'.$orderFields[0]['barnes-noble-link'].'" target="_blank" class="barnes"><img src="'.$barnes[0].'" alt="Barnes and Noble"/></a>';
-									}
-								?>
+								@isset($order_fields['litfire-link'])
+									<a href="{{ $order_fields['litfire-link'] }}" target="_blank"
+									   class="litfire">{!! wp_get_attachment_image($order_fields['litfire-image'], 'full') !!}
+									</a>
+								@endisset
+
+								@isset($order_fields['amazon-link'])
+									<a href="{{ $order_fields['amazon-link'] }}" target="_blank" class="amazon">
+										{!! wp_get_attachment_image($order_fields['amazon-image'], 'full') !!}
+									</a>
+								@endisset
+
+								@isset($order_fields['barnes-noble-link'])
+									<a href="{{ $order_fields['barnes-noble-link'] }}" target="_blank" class="barnes-noble">
+										{!! wp_get_attachment_image($order_fields['barnes-noble-image'], 'full') !!}
+									</a>
+								@endisset
 							</div>
+							<! /.book-store -->
 						</div>
 					</div>
 					<div class="col-md-7">
 						<div class="excerpt-content" data-aos="fade-up" data-aos-duration="1000">
-							<h2><?php echo get_the_title(15); ?></h2>
+							<h2>{{ get_the_title(15) }}</h2>
 							<h3><strong>Excerpt Content Only</strong></h3>
-							<?php
-								$content_post = get_post(15);
-								$content = $content_post->post_content;
-								$content = apply_filters('the_content', $content);
-								$content = str_replace(']]>', ']]&gt;', $content);
-								echo wpautop($content);
-							?>
+							{!! wpautop(get_post(13)->post_content) !!}
 						</div>
 						<div class="excerpt-carousel" data-aos="fade-up" data-aos-duration="1000">
 							<h3><strong>Excerpt Content Carousel</strong></h3>
 							<div id="excerpt-carou2" class="carousel slide" data-ride="carousel" data-interval="false">
-								<?php
-									$contents = get_post_meta(15,'excerpt_content', true );
-								?>
 								<div class="carousel-inner">
-									<?php
-										$count = 0;
-										$status= '';
-										$carouIn = '<ol class="carousel-indicators">';
-										foreach ( $contents as $content ) :
-											if($count == 0)
-							               {
-							                  $status = 'active';
-							               }
-							               else{
-							                  $status = '';
-							               }
-											$carouIn.= '<li data-target="#excerpt-carou2" data-slide-to="'.$count.'" class="'.$status.'"></li>';
-											echo '<div class="carousel-item '.$status.'">
-													'. $content['excerpt-content'] .'
-												</div>';
-										$count++;
-										endforeach;
-										$carouIn.= '</ol>';
-									?>
-								</div>
-								<?php
-									echo $carouIn;
-								?>
+									@foreach(get_post_meta(15, 'excerpt_content', true) as $content)
+										<div class="carousel-item @if ($loop->first) active @endif">
+											{!! $content['excerpt-content'] !!}
+										</div>
+									@endforeach
+								</div><!-- /.carousel-innner -->
+
+								<ol class="carousel-indicators">
+									@foreach(get_post_meta(15, 'excerpt_content', true) as $content)
+										<li data-target="#excerpt-carou2" data-slide-to="{{ $loop->index }}" @if ($loop->first) class="active" @endif></li>
+									@endforeach
+								</ol>
 							</div>
 						</div>
 					</div>
@@ -275,33 +195,25 @@
 			</div>
 		</div>
 	</section>
-		
-	
+
 	<section id="contact">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-5">
 					<div class="text-social-cont" data-aos="fade-up" data-aos-duration="1000">
 						<h2>{{ get_the_title(19) }}</h2>
-			            <?php
-							$content_post = get_post(19);
-							$content = $content_post->post_content;
-							$content = apply_filters('the_content', $content);
-							$content = str_replace(']]>', ']]&gt;', $content);
-							echo wpautop($content);
-						?>
-			            <?php  
-			                wp_nav_menu(['menu' =>  'Footer Social Menu', 'menu_class' => 'footer-social']); 
-			            ?>
+
+						{!! wpautop(get_post(19)->post_content) !!}
+						{!! wp_nav_menu(['menu' => 'Footer Social Menu', 'menu_class' => 'footer-social']) !!}
 					</div>
 				</div>
 				<div class="col-md-7">
 					<div class="form-cont" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
-			        	<?php echo do_shortcode('[contact-form-7 id="4" title="Contact form"]'); ?>
+						{!! do_shortcode('[contact-form-7 id="4" title="Contact form"]') !!}
 					</div>
 				</div>
 			</div>
 		</div>
 	</section>
-  @endwhile
+	@endwhile
 @endsection
